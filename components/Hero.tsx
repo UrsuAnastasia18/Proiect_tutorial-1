e98fc4102309d2eff7a4f96f2b7a34a53c343ae6
Ursu <anastasiaursu76@gmail.com>
2024-09-08 22:36:21 +0300

    Adding public files

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,5 +1,6 @@
 "use client";
 import Image from 'next/image';
+
 import CustomButton from './CustomButton';
 
 const Hero = () => {
@@ -27,7 +28,7 @@ const Hero = () => {
       </div>
       <div className="hero__image-container">
         <div className="hero__image">
-          <Image src="/hero.png" alt="hero"
+          <Image src= "/hero.png" alt="hero"
           fill className="object-contain"/>
          
         <div className="hero__image-overlay"/>
