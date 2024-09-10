f04d649942a07c33989e7f21ffaa2003617d0ed3
Ursu <anastasiaursu76@gmail.com>
2024-09-11 00:23:53 +0300

    "Car images" api key doesn't work

--- components/CarDetails.tsx
+++ components/CarDetails.tsx
@@ -4,6 +4,7 @@ import Image from 'next/image';
 import {Fragment} from 'react';
 import {Dialog, Transition} from '@headlessui/react';
 import {CarProps} from '@/types';
+import { generateCarImageUrl } from '@/utils';
 
 interface CarDetailsProps{
     isOpen:boolean;
@@ -63,15 +64,15 @@ leaveTo="opacity-0 scale-95"
 
 <div className = "flex gap-3">
 <div className="flex-1 relative w-full h-24 bg-primary-blue-100 rounded-lg">
-<Image src="/hero.png" alt="car model" fill priority className="object-contain"/>
+<Image src={generateCarImageUrl(car,'29')} alt="car model" fill priority className="object-contain"/>
 </div>
 
 <div className="flex-1 relative w-full h-24 bg-primary-blue-100 rounded-lg">
-<Image src="/hero.png" alt="car model" fill priority className="object-contain"/>
+<Image src={generateCarImageUrl(car,'33')} alt="car model" fill priority className="object-contain"/>
 </div>
 
 <div className="flex-1 relative w-full h-24 bg-primary-blue-100 rounded-lg">
-<Image src="/hero.png" alt="car model" fill priority className="object-contain"/>
+<Image src={generateCarImageUrl(car,'13')} alt="car model" fill priority className="object-contain"/>
 </div>
 </div>
 </div>
